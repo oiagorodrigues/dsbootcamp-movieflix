0ab6a6ed74abb7442b5d6b424ce829cbb487272e
Iago Rodrigues <yagomello16@gmail.com>
2021-07-03 19:23:05 -0300

    feat: Movie, Genre entity, relations

---
+++ src/main/java/com/devsuperior/movieflix/entities/Genre.java
@@ -0,0 +1,64 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_genre")
+public class Genre implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "genre")
+    private List<Movie> movies = new ArrayList<>();
+
+    public Genre() {
+    }
+
+    public Genre(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        return getId().equals(genre.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
---
+++ src/main/java/com/devsuperior/movieflix/entities/Movie.java
@@ -0,0 +1,119 @@
+package com.devsuperior.movieflix.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_movie")
+public class Movie implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String subTitle;
+    private Integer year;
+    private String imgUrl;
+    private String synopsis;
+
+    @ManyToOne
+    private Genre genre;
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String title, String subTitle, Integer year, String imgUrl, String synopsis, Genre genre) {
+        this.id = id;
+        this.title = title;
+        this.subTitle = subTitle;
+        this.year = year;
+        this.imgUrl = imgUrl;
+        this.synopsis = synopsis;
+        this.genre = genre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (!getId().equals(movie.getId())) return false;
+        if (getTitle() != null ? !getTitle().equals(movie.getTitle()) : movie.getTitle() != null) return false;
+        if (getSubTitle() != null ? !getSubTitle().equals(movie.getSubTitle()) : movie.getSubTitle() != null)
+            return false;
+        if (getYear() != null ? !getYear().equals(movie.getYear()) : movie.getYear() != null) return false;
+        if (getImgUrl() != null ? !getImgUrl().equals(movie.getImgUrl()) : movie.getImgUrl() != null) return false;
+        if (getSynopsis() != null ? !getSynopsis().equals(movie.getSynopsis()) : movie.getSynopsis() != null)
+            return false;
+        return getGenre() != null ? getGenre().equals(movie.getGenre()) : movie.getGenre() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
+}
